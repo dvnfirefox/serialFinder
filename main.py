de0fa97aea32574244436e7bab4cf2d8c22d0af1
dvnfirefox <dvnfirefox123@gmail.com>
2025-04-26 19:04:45 -0400

    working without ui

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
-import pandas as pd
-import sqlite3
-
-excelInput = '2015-2021.xlsx'
-try:
-    dbcon = sqlite3.connect('order.db')
-    cursor = dbcon.cursor()
-    cursor.execute("CREATE TABLE serialNumberRaw(invoice, serialNumber)")
-    cursor.execute("CREATE TABLE serialNumber(invoice, serialNumber)")
-except Exception as e:
-    print(f"error : {e}")
 
 
+import database as db
 
-try:
-    sheet2020 = pd.read_excel(excelInput, sheet_name='2020')
-    sheet2020['Serial #'] = sheet2020['Serial #'].astype(str)
-    sheet2020['Invoice'] = sheet2020['Invoice'].astype(str)
-    serialRaw = sheet2020[['Invoice', 'Serial #']]
-    for index, row in serialRaw.iterrows():
-        invoice = row['Invoice']
-        serial = row['Serial #']
-        if ("nan" not in invoice and "nan" not in serial
-        ):
-            invoice = invoice.split('.', 1)[0]
-            print(invoice, serial)
-            cursor.execute("INSERT INTO serialNumberRaw VALUES(?, ?)", (invoice, serial))
-    dbcon.commit()
-except Exception as e:
-    print(f"error : {e}")
+if db.createTables():
+    db.rawSerialReader('2020')
+    db.rawSerialReader('2021')
+    db.rawSerialReader('2022')
+    db.multipleSerial()
+    db.serialSeries()
+    db.simpleSerial()
 
 
+while 1:
+    print('enter serial number')
+    userInput = input()
+    result = db.serialNumberSearch(userInput)
+    for row in result:
+        print('invoice: ' + row[0] + ' serial number: ' + row[1] + ' date: ' + row[2])
